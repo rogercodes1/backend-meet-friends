b526809dbc7ceffa2f8d6c6c0a4ad20680af5909
rogercodes1 <roger.ucsb@gmail.com>
2018-06-28 19:42:28 -0400

    renamed sessions files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'session/index'
-  get 'session/create'
-  get 'session/show'
+  get 'sessions/index'
+  get 'sessions/create'
+  get 'sessions/show'
   namespace :api do
     namespace :v1 do
       resources :users, only:[:index, :create, :show, :edit]
