57da38c2fc00558c22a36918d2b4fbb2be18c7e3
rogercodes1 <roger.ucsb@gmail.com>
2018-07-04 22:06:22 -0400

    updated sessions data.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,20 @@
 class SessionsController < ApplicationController
-
   def create
+    @user = User.find_by(email: params['email'])
+    if @user && @user.authenticate(params['password'])
+      token = generate_token
 
-     @user = User.find_by(email: params["email"])
-
-     if (@user && @user.authenticate(params["password"]))
-       token = generate_token
-
-        render json: {
-           email: @user.email,
-           id: @user.id,
-           token: token,
-           status: :accepted
-         }
-      else
-        render json: {
-           errors: "Login credentials are incorrect, Try again.",
-           status: :unauthorized
-        }
-     end
+      render json: {
+        email: @user.email,
+        id: @user.id,
+        token: token,
+        status: :accepted
+      }
+    else
+      render json: {
+        errors: 'Login credentials are incorrect, Try again.',
+        status: :unauthorized
+      }
+    end
   end
-
-
-
 end
