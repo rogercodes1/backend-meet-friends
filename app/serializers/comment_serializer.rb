025be6ee4b8e78ef0890dd34f944cc9636cfdf9a
rogercodes1 <roger.ucsb@gmail.com>
2018-07-10 09:34:04 -0400

    added created at attribute to comments serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :event_id, :user_id
+  attributes :id, :comment, :event_id, :user_id, :created_at
 
   belongs_to :event
 
