500b992734e0820e3be54767e0fee04b888e3c5f
rogercodes1 <roger.ucsb@gmail.com>
2018-07-27 15:38:33 -0400

    removed cors testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery with: :null_session
-   before_filter :current_user, :cors_preflight_check
-   after_filter :cors_set_access_control_headers
+   
    # skip_before_action :verify_authenticity_token
 
   def get_secret
@@ -76,30 +75,5 @@ class ApplicationController < ActionController::Base
   end
 
 
- # For all responses in this controller, return the CORS access control headers.
-
- def cors_set_access_control_headers
-   headers['Access-Control-Allow-Origin'] = '*'
-   headers['Access-Control-Allow-Headers'] = '*'
-   headers['Access-Control-Max-Age'] = "1728000"
-   headers['Access-Control-Allow-Origin'] = '*'
-   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-   headers['Access-Control-Request-Method'] = '*'
-   # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
- end
-
- # If this is a preflight OPTIONS request, then short-circuit the
- # request, return only the necessary headers and return an empty
- # text/plain.
-
- def cors_preflight_check
-   if request.method == :options
-     headers['Access-Control-Allow-Origin'] = '*'
-     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-     headers['Access-Control-Allow-Headers'] = '*'
-     headers['Access-Control-Max-Age'] = '1728000'
-     render :text => '', :content_type => 'text/plain'
-   end
- end
 
 end
